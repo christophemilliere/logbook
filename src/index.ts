df88759875964b416c0b121cb71ae4dd2f9f0fc7
christophemilliere <christophemilliere93@gmail.com>
2020-03-28 22:57:30 +0100

    add asset

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,37 @@ import AuthRoutes from "./auth/route";
 import { checkJwt } from "./middlewares/checkJwt";
 import CategoryRoutes from "./category/route";
 import VideoRoutes from "./video/route";
+import path from "path";
 
+console.log({ __dirname });
 
 createConnection().then(async connection => {
-	const options = {
-		explorer: true
-	};
-	const app = express();
+  const options = {
+    explorer: true
+  };
+  const app = express();
 
-	app.use(bodyParser.json());
-	app.use(cors());
-	// app.use(helmet());
+  app.use(bodyParser.json());
+  app.use(cors());
+  // app.use(helmet());
 
-	app.use("/utilisateurs", userRoutes);
-	// app.use("/activities", ActivityRoute);
-	app.use("/authentification", AuthRoutes);
-	// app.use("/recherche", SearchRoutes);
-	// app.use("/email", EmailRoutes);
-	app.use("/categories", checkJwt, CategoryRoutes);
-	// app.use("/bookmarks", BookmarksRoutes);
-	app.use("/video", checkJwt, VideoRoutes);
-	//expressOasGenerator.init(app, {});
+  app.use("/utilisateurs", userRoutes);
+  // app.use("/activities", ActivityRoute);
+  app.use("/authentification", AuthRoutes);
+  // app.use("/recherche", SearchRoutes);
+  // app.use("/email", EmailRoutes);
+  app.use("/categories", checkJwt, CategoryRoutes);
+  // app.use("/bookmarks", BookmarksRoutes);
+  app.use("/video", checkJwt, VideoRoutes);
+  //expressOasGenerator.init(app, {});
+  app.use("/static", express.static(__dirname + "/public"));
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname + "/public/index.html"));
+  });
 
-	//app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  //app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-	app.listen(5000, function () {
-		console.log("Example app listening on port 5000!");
-	});
+  app.listen(5000, function() {
+    console.log("Example app listening on port 5000!");
+  });
 });
