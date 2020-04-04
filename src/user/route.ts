c71e49b79ea75f47fec7ac1c7c56e3d9f9b008e2
christophemilliere <christophemilliere93@gmail.com>
2020-04-04 15:57:26 +0200

    update

--- src/user/route.ts
+++ src/user/route.ts
@@ -3,7 +3,7 @@ import UserController from "./controller";
 
 const router = Router();
 //Get all users
-router.get("/", UserController.listAll);
+// router.get("/", UserController.listAll);
 //Get one user
 router.get("/:id([0-9]+)", UserController.getOneById);
 router.post("/", UserController.newUser);
